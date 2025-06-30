8375f15fc73cfb370001beb2300bd227c591901b
lauirf <592184299@qq.com>
2025-06-30 11:08:33 +0800

    set base path for deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/web_player/',
   plugins: [tailwindcss(), react()],
   server: {
     proxy: {
